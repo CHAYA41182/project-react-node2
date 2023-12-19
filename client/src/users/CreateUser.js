fe238b44a94c83470df5ad6637dbd57e4fa1f9de
chaya kremer <chaya41182@gmail.com>
2023-12-19 17:42:00 +0200

    Refactor CreateUser component to use useRef for form inputs and add phone to createUser component

--- client/src/users/CreateUser.js
+++ client/src/users/CreateUser.js
@@ -3,20 +3,28 @@ import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { useRef } from "react";
 
 const CreateUser = () => {
     const navigate = useNavigate();
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const isAdminRef = useRef();
+    const phoneRef = useRef();
+
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const formData = new FormData(e.target);
         const roles = []
-        formData.get("isAdmin")?roles.push("admin", "user"):roles.push("user")
 
         const user = {
-            name: formData.get('name'),
-            email: formData.get('email'),
-            password: formData.get('password'),
-            roles:roles
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            roles: roles,
+            phone: phoneRef.current.value
+
         };
         
        await axios.post("http://localhost:7001/api/users",user).then(()=>{
@@ -34,20 +42,25 @@ const CreateUser = () => {
             <form className="create-form" onSubmit={handleSubmit}>
                     <div className="name-container">
                     <label htmlFor="name">Name</label>
-                    <input type="text" name="name" className="form-control" required />
+                    <input type="text" name="name" className="form-control" required ref={nameRef} />
                     </div>
                     <div className="email-container">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" className="form-control" required />
+                    <input type="email" name="email" className="form-control" required ref={emailRef} />
                     </div>
                     <div className="password-container">
                     <label htmlFor="password">Password</label>
-                    <input type="text" name="password" className="form-control" required />
+                    <input type="text" name="password" className="form-control" required ref={passwordRef} />
+                    </div>
+                    <div className="phone-container">
+                    <label htmlFor="phone">Phone</label>
+                    <input type="text" name="phone" className="form-control" ref={phoneRef} />
                     </div>
                     <div className="roles-container">
-                    <input type="checkbox" name="isAdmin" className="form-check-input" />
+                    <input type="checkbox" name="isAdmin" className="form-check-input" ref={isAdminRef} />
                     <label htmlFor="isAdmin">isAdmin</label>
                     </div>
+
                     
                     <div className="form-group">
                         <button type="submit" className="btn btn-primary">Create</button>
